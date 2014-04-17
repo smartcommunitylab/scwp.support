c6492cb3cfb07c06cbf8c88fa0ccea1fe198630a
Andrea Panizza <panizza01@gmail.com>
2014-04-17 16:49:17 +0200

    added mbs project

---
+++ SmartCampusWPSupport/MobilityServiceLibrary/PublicTransportLibrary.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Models.MobilityService.PublicTransport;
+using System.Threading.Tasks;
+
+
+namespace MobilityServiceLibrary
+{
+    public class MobilityLibrary
+    {
+        WebClient WebCli;
+    string AccessToken;
+
+    public MobilityLibrary(string accessToken)
+    {
+      this.AccessToken = accessToken;
+      WebCli = new WebClient();
+    }
+
+    /*public async Task<Route> GetBasicProfile()
+    {
+      WebCli.Headers["Authorization"] = string.Format("Bearer {0}", AccessToken);
+      WebCli.Headers["Accept"] = "application/json";
+    string JSONResult = await WebCli.DownloadStringTaskAsync(ProfileUriHelper.BuildUriForBasicProfile());
+
+      return Newtonsoft.Json.JsonConvert.DeserializeObject<BasicProfile>(JSONResult);
+    }*/
+    }
+}
---
+++ SmartCampusWPSupport/MobilityServiceLibrary/PublicTransportUriHelper.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+
+namespace MobilityServiceLibrary
+{
+    public static class PublicTransportUriHelper
+    {
+        static string BaseUrl = "https://vas-dev.smartcampuslab.it/core.mobility/";
+        static string GetRoutes = "getroutes";
+        static string GetStops = "getstops";
+        static string GetTimetable = "gettimetable";
+        
+
+
+        public static Uri GetRoutesUri(string agencyId)
+        {
+            UriBuilder ub = new UriBuilder(string.Format("{0}/{1}", BaseUrl, GetRoutes, agencyId));
+            return ub.Uri;
+        }
+
+        public static Uri GetStopsUri(string agencyId, string routeId)
+        {
+            UriBuilder ub = new UriBuilder(string.Format("{0}/{1}/{2}", BaseUrl, GetStops, agencyId, routeId));
+            return ub.Uri;
+        }
+
+        public static Uri GetTimetableUri(string agencyId, string routeId, string stopId)
+        {
+            UriBuilder ub = new UriBuilder(string.Format("{0}/{1}/{2}", BaseUrl, GetTimetable, agencyId, routeId, stopId));
+            return ub.Uri;
+        }
+
+
+    }
+}
