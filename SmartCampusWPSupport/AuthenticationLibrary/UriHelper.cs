3c237924a4ae97192a4968c7e078edc363c7da55
TheFerrango <llorenzo@outlook.com>
2014-04-17 10:56:56 +0200

    added net library
    added auth library with many helpers

---
+++ SmartCampusWPSupport/AuthenticationLibrary/UriHelper.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+
+namespace AuthenticationLibrary
+{
+  public static class UriHelper
+  {
+    static string BaseUrl = "https://vas-dev.smartcampuslab.it/";
+    static string GetCodeUrl = "aac/eauth/authorize";
+    static string GetTokenUrl = "aac/oauth/token";
+    static string RevokeTokenUrl = "aac/eauth/revoke";
+
+    public static Uri BuildUriForCode(string clientId, string redirectUrl)
+    {
+      UriBuilder ub = new UriBuilder(string.Format("{0}{1}",BaseUrl,GetCodeUrl));
+      
+      Dictionary<string, string> StringQuery = new Dictionary<string, string>();
+      StringQuery["client_id"] = clientId;
+      StringQuery["response"] = "code";
+      StringQuery["redirect_uri"] = redirectUrl;
+      ub.Query = QueryHelper.DictionaryToQuery(StringQuery);
+
+      return ub.Uri;
+    }
+
+    public static Uri BuildUriForToken(string code, string clientSecret, string clientId, string redirectUrl)
+    {
+      UriBuilder ub = new UriBuilder(string.Format("{0}{1}", BaseUrl, GetTokenUrl));
+
+      Dictionary<string, string> StringQuery = new Dictionary<string, string>();
+      StringQuery["client_id"] = clientId;
+      StringQuery["client_secret"] = clientSecret;
+      StringQuery["code"] = code;
+      StringQuery["redirect_uri"] = redirectUrl;
+      StringQuery["grant_type"] = "authorization_code";
+
+      ub.Query = QueryHelper.DictionaryToQuery(StringQuery);
+
+      return ub.Uri;
+    }
+
+    public static Uri BuildUriForRefreshToken(string refreshToken, string clientSecret, string clientId)
+    {
+      UriBuilder ub = new UriBuilder(string.Format("{0}{1}", BaseUrl, GetTokenUrl));
+
+      Dictionary<string, string> StringQuery = new Dictionary<string, string>();
+      StringQuery["client_id"] = clientId;
+      StringQuery["client_secret"] = clientSecret;
+      StringQuery["refresh_token"] = refreshToken;
+      StringQuery["grant_type"] = "refresh_token";
+
+      ub.Query = QueryHelper.DictionaryToQuery(StringQuery);
+
+      return ub.Uri;
+    }
+
+    public static Uri BuildUriForRevokeToken(string accessToken)
+    {
+      UriBuilder ub = new UriBuilder(string.Format("{0}{1}/{2}", BaseUrl, RevokeTokenUrl, accessToken));
+      return ub.Uri;
+    }
+  }
+}
---
+++ SmartCampusWPSupport/Models/AuthorizationService/TokenModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.AuthorizationService
+{
+  class TokenModel
+  {
+    [JsonProperty("access_token")]
+    public string AccessToken { get; set; }
+
+    [JsonProperty("token_type")]
+    public string AccessToken { get; set; }
+
+    [JsonProperty("refresh_token")]
+    public string AccessToken { get; set; }
+
+    [JsonProperty("expires_in")]
+    public int AccessToken { get; set; }
+
+    [JsonProperty("scope")]
+    public string AccessToken { get; set; }
+  }
+}
