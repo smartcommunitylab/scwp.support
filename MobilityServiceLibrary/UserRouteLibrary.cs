68980a0b5d9d52e96f8bbcb6cd1ebea81decc688
TheFerrango <llorenzo@outlook.com>
2014-04-22 11:00:16 +0200

    moar documentation
    *added documentation for RoutePlanningUriHelper
    *added documentation for UserRouteUriHelper
    *added documentation for UserRoute
    *added wrapping of recurrent journey API to UserRoute

--- MobilityServiceLibrary/UserRouteLibrary.cs
+++ MobilityServiceLibrary/UserRouteLibrary.cs
@@ -10,11 +10,18 @@ using System.Threading.Tasks;
 
 namespace MobilityServiceLibrary
 {
+  /// <summary>
+  /// Class that wraps the User Route APIs in an easy to use way
+  /// </summary>
   public class UserRouteLibrary
   {
     HttpClient httpCli;
     string accessToken;
 
+    /// <summary>
+    /// Main constructor, to use always
+    /// </summary>
+    /// <param name="accessToken">The SmartCampus-issued access token</param>
     public UserRouteLibrary(string accessToken)
     {
       this.accessToken = accessToken;
@@ -23,6 +30,11 @@ namespace MobilityServiceLibrary
 
     #region Single journey
 
+    /// <summary>
+    /// Asyncronous method that allows the user to store a single journey
+    /// </summary>
+    /// <param name="basIti">The instance of an object representing the journey to store</param>
+    /// <returns>An instance of the stored journey, as stored on the server</returns>
     public async Task<BasicItinerary> SaveSingleJourney(BasicItinerary basIti)
     {
       string toPost = JsonConvert.SerializeObject(basIti);
@@ -36,6 +48,11 @@ namespace MobilityServiceLibrary
       return JsonConvert.DeserializeObject<BasicItinerary>(await res.Content.ReadAsStringAsync());
     }
 
+    /// <summary>
+    /// Asyncronous method that allows the user to read a single journey stored by the user
+    /// </summary>
+    /// <param name="journeyId">The unique identifier for the desired journey</param>
+    /// <returns>An instance of the desired stored journey</returns>
     public async Task<BasicItinerary> ReadSingleJourney(int journeyId)
     {
       httpCli.DefaultRequestHeaders.Add("Accept", "application/json");
@@ -45,6 +62,10 @@ namespace MobilityServiceLibrary
       return JsonConvert.DeserializeObject<BasicItinerary>(res);
     }
 
+    /// <summary>
+    /// Asyncronous method that allows the user to read all single journeys stored by the user
+    /// </summary>
+    /// <returns>A list containing the instances of all the stored journeys</returns>
     public async Task<List<BasicItinerary>> ReadAllSingleJourneys()
     {
       httpCli.DefaultRequestHeaders.Add("Accept", "application/json");
@@ -54,20 +75,127 @@ namespace MobilityServiceLibrary
       return JsonConvert.DeserializeObject<List<BasicItinerary>>(res);
     }
 
+    /// <summary>
+    /// Asyncronous method that allows the user to switch journey monitoring on or off.
+    /// </summary>
+    /// <param name="journeyId">The unique identifier for the desired journey</param>
+    /// <param name="monitor">The status to which monitoring will be set</param>
+    /// <returns>The updated monitoring status</returns>
     public async Task<bool> SetMonitorSingleJourney(int journeyId, bool monitor)
     {
       httpCli.DefaultRequestHeaders.Add("Accept", "application/json");
       httpCli.DefaultRequestHeaders.Add("Authorization", string.Format("Bearer {0}", accessToken));
-      var res = await httpCli.GetStringAsync(UserRouteUriHelper.GetReadAllSingleJourneysUri());
+      var res = await httpCli.GetStringAsync(UserRouteUriHelper.GetMonitorSingleJourneyUri(journeyId, monitor));
 
       return Convert.ToBoolean(res);
     }
 
+    /// <summary>
+    /// Asyncronous method that allows the user to delete a stored journey
+    /// </summary>
+    /// <param name="journeyId">The unique identifier for the desired journey</param>
+    /// <returns>The result of the deleting operation</returns>
     public async Task<bool> DeleteSingleJourney(int journeyId)
     {
       httpCli.DefaultRequestHeaders.Add("Accept", "application/json");
       httpCli.DefaultRequestHeaders.Add("Authorization", string.Format("Bearer {0}", accessToken));
-      var res = await httpCli.DeleteAsync(UserRouteUriHelper.GetReadAllSingleJourneysUri());
+      var res = await httpCli.DeleteAsync(UserRouteUriHelper.GetDeleteSingleJourneyUri(journeyId));
+
+      return Convert.ToBoolean(await res.Content.ReadAsStringAsync());
+    }
+
+
+    #endregion
+
+    #region Recurent journey
+
+    /// <summary>
+    /// Asyncronous method that allows the user to store a recurrent journey
+    /// </summary>
+    /// <param name="basIti">The instance of an object representing the journey to store</param>
+    /// <returns>An instance of the stored recurrent journey, as stored on the server</returns>
+    public async Task<BasicRecurrentJourney> SaveRecurrentJourney(BasicRecurrentJourney basIti)
+    {
+      string toPost = JsonConvert.SerializeObject(basIti);
+
+      StringContent sc = new StringContent(toPost);
+      httpCli.DefaultRequestHeaders.Add("Accept", "application/json");
+      httpCli.DefaultRequestHeaders.Add("Authorization", string.Format("Bearer {0}", accessToken));
+
+      var res = await httpCli.PostAsync(UserRouteUriHelper.GetSaveRecurrentJourneyUri(), sc);
+
+      return JsonConvert.DeserializeObject<BasicRecurrentJourney>(await res.Content.ReadAsStringAsync());
+    }
+
+    /// <summary>
+    /// Asyncronous method that allows the user to read a recurrent journey stored by the user
+    /// </summary>
+    /// <param name="journeyId">The unique identifier for the desired journey</param>
+    /// <returns>An instance of the desired stored recurrent journey</returns>
+    public async Task<BasicRecurrentJourney> ReadRecurrentJourney(int journeyId)
+    {
+      httpCli.DefaultRequestHeaders.Add("Accept", "application/json");
+      httpCli.DefaultRequestHeaders.Add("Authorization", string.Format("Bearer {0}", accessToken));
+      var res = await httpCli.GetStringAsync(UserRouteUriHelper.GetReadRecurrentJourneyUri(journeyId));
+
+      return JsonConvert.DeserializeObject<BasicRecurrentJourney>(res);
+    }
+
+    /// <summary>
+    /// Asyncronous method that allows the user to read all recurrent journeys stored by the user
+    /// </summary>
+    /// <returns>A list containing the instances of all the stored recurrent journeys</returns>
+    public async Task<List<BasicRecurrentJourney>> ReadAllRecurrentJourneys()
+    {
+      httpCli.DefaultRequestHeaders.Add("Accept", "application/json");
+      httpCli.DefaultRequestHeaders.Add("Authorization", string.Format("Bearer {0}", accessToken));
+      var res = await httpCli.GetStringAsync(UserRouteUriHelper.GetReadAllRecurrentJourneysUri());
+
+      return JsonConvert.DeserializeObject<List<BasicRecurrentJourney>>(res);
+    }
+
+    /// <summary>
+    /// Asyncronous method that allows the user to switch journey monitoring on or off.
+    /// </summary>
+    /// <param name="journeyId">The unique identifier for the desired recurrent journey</param>
+    /// <param name="monitor">The status to which monitoring will be set</param>
+    /// <returns>The updated monitoring status</returns>
+    public async Task<bool> SetMonitorRecurrentJourney(int journeyId, bool monitor)
+    {
+      httpCli.DefaultRequestHeaders.Add("Accept", "application/json");
+      httpCli.DefaultRequestHeaders.Add("Authorization", string.Format("Bearer {0}", accessToken));
+      var res = await httpCli.GetStringAsync(UserRouteUriHelper.GetMonitorReccurrentJourneyUri(journeyId, monitor));
+
+      return Convert.ToBoolean(res);
+    }
+
+    /// <summary>
+    /// Asyncronous method that allows the user to switch journey monitoring on or off.
+    /// </summary>
+    /// <param name="journeyId">The unique identifier for the desired recurrent journey</param>
+    /// <param name="basIti">The instance the updated journey to store</param>
+    /// <returns>The result of the update operation</returns>
+    public async Task<bool> UpdateRecurrentJourney(int journeyId, BasicRecurrentJourney basIti)
+    {
+      string toPost = JsonConvert.SerializeObject(basIti);
+      StringContent sc = new StringContent(toPost);
+      httpCli.DefaultRequestHeaders.Add("Accept", "application/json");
+      httpCli.DefaultRequestHeaders.Add("Authorization", string.Format("Bearer {0}", accessToken));
+      var res = await httpCli.PutAsync(UserRouteUriHelper.GetUpdateRecurrentJourneyUri(journeyId), sc);
+
+      return Convert.ToBoolean(res);
+    }
+
+    /// <summary>
+    /// Asyncronous method that allows the user to delete a stored recurrent journey
+    /// </summary>
+    /// <param name="journeyId">The unique identifier for the desired journey</param>
+    /// <returns>The result of the deleting operation</returns>
+    public async Task<bool> DeleteRecurrentJourney(int journeyId)
+    {
+      httpCli.DefaultRequestHeaders.Add("Accept", "application/json");
+      httpCli.DefaultRequestHeaders.Add("Authorization", string.Format("Bearer {0}", accessToken));
+      var res = await httpCli.DeleteAsync(UserRouteUriHelper.GetDeleteRecurrentJourneyUri(journeyId));
 
       return Convert.ToBoolean(await res.Content.ReadAsStringAsync());
     }
--- MobilityServiceLibrary/UserRouteUriHelper.cs
+++ MobilityServiceLibrary/UserRouteUriHelper.cs
@@ -6,6 +6,10 @@ using System.Threading.Tasks;
 
 namespace MobilityServiceLibrary
 {
+  /// <summary>
+  /// Helper class for the User Route library, contains static functions that 
+  /// generate already-formatted URIs for using within the User Route library
+  /// </summary>
   public static class UserRouteUriHelper
   {
     static string baseUrl = "https://vas-dev.smartcampuslab.it/core.mobility";
@@ -14,29 +18,53 @@ namespace MobilityServiceLibrary
 
     #region Single journey
 
+    /// <summary>
+    /// Creates a formatted URI to store a planned journey
+    /// </summary>
+    /// <returns>A ready to use URI to use in order to store a planned journey to the server</returns>
     public static Uri GetSaveSingleJourneyUri()
     {
       UriBuilder ub = new UriBuilder(string.Format("{0}/{1}", baseUrl, itineraryUrl));
       return ub.Uri;
     }
 
+    /// <summary>
+    /// Creates a formatted URI to retrieve a planned journey
+    /// </summary>
+    /// <param name="journeyId">The unique identifier for the desired journey</param>
+    /// <returns>A ready to use URI for retrieving a specific journey from the server</returns>
     public static Uri GetReadSingleJourneyUri(int journeyId)
     {
       UriBuilder ub = new UriBuilder(string.Format("{0}/{1}/{2}", baseUrl, itineraryUrl, journeyId));
       return ub.Uri;
     }
 
+    /// <summary>
+    /// Creates a formatted URI to retrieve all planned journeys
+    /// </summary>
+    /// <returns>A ready to use URI for retrieving all journeys from the server</returns>
     public static Uri GetReadAllSingleJourneysUri()
     {
       return GetSaveSingleJourneyUri();
     }
 
+    /// <summary>
+    /// Creates a formatted URI to set the monitoring status on a specific journey
+    /// </summary>
+    /// <param name="journeyId">The unique identifier for the desired journey</param>
+    /// <param name="monitor">The value indicating whether to start or stop monitoring the journey</param>
+    /// <returns>A ready to use URI for setting the monitoring status on a specific journey</returns>
     public static Uri GetMonitorSingleJourneyUri(int journeyId, bool monitor)
     {
       UriBuilder ub = new UriBuilder(string.Format("{0}/{1}/{2}/monitor/{3}", baseUrl, itineraryUrl, journeyId, monitor));
       return ub.Uri;
     }
 
+    /// <summary>
+    /// Creates a formatted URI to delete a specific journey
+    /// </summary>
+    /// <param name="journeyId">The unique identifier for the desired journey</param>
+    /// <returns>A ready to use URI for deleting a specific journey</returns>
     public static Uri GetDeleteSingleJourneyUri(int journeyId)
     {
       return GetReadSingleJourneyUri(journeyId);
@@ -46,34 +74,63 @@ namespace MobilityServiceLibrary
 
     #region Recurrent Journey
 
+    /// <summary>
+    /// Creates a formatted URI to store a planned recurrent journey
+    /// </summary>
+    /// <returns>A ready to use URI to use in order to store a planned recurrent journey to the server</returns>
     public static Uri GetSaveRecurrentJourneyUri()
     {
       UriBuilder ub = new UriBuilder(string.Format("{0}/{1}", baseUrl, recurrentUrl));
       return ub.Uri;
     }
 
+    /// <summary>
+    /// Creates a formatted URI to retrieve a planned recurrent journey
+    /// </summary>
+    /// <param name="journeyId">The unique identifier for the desired recurrent journey</param>
+    /// <returns>A ready to use URI for retrieving a specific recurrent journey from the server</returns>
     public static Uri GetReadRecurrentJourneyUri(int journeyId)
     {
       UriBuilder ub = new UriBuilder(string.Format("{0}/{1}/{2}", baseUrl, recurrentUrl, journeyId));
       return ub.Uri;
     }
 
+    /// <summary>
+    /// Creates a formatted URI to retrieve all planned recurrent journeys
+    /// </summary>
+    /// <returns>A ready to use URI for retrieving all journeys from the server</returns>
     public static Uri GetReadAllRecurrentJourneysUri()
     {
       return GetSaveRecurrentJourneyUri();
     }
 
+    /// <summary>
+    /// Creates a formatted URI to set the monitoring status on a specific recurrent journey
+    /// </summary>
+    /// <param name="journeyId">The unique identifier for the desired recurrent journey</param>
+    /// <param name="monitor">The value indicating whether to start or stop monitoring the recurrent journey</param>
+    /// <returns>A ready to use URI for setting the monitoring status on a specific recurrent journey</returns>
     public static Uri GetMonitorReccurrentJourneyUri(int journeyId, bool monitor)
     {
       UriBuilder ub = new UriBuilder(string.Format("{0}/{1}/{2}/monitor/{3}", baseUrl, recurrentUrl, journeyId, monitor));
       return ub.Uri;
     }
 
+    /// <summary>
+    /// Creates a formatted URI to update a specific recurrent journey already stored to the server
+    /// </summary>
+    /// <param name="journeyId">The unique identifier for the desired journey</param>
+    /// <returns>A ready to use URI for updating a specific recurrent journey</returns>
     public static Uri GetUpdateRecurrentJourneyUri(int journeyId)
     {
       return GetReadRecurrentJourneyUri(journeyId);
     }
 
+    /// <summary>
+    /// Creates a formatted URI to delete a specific recurrent journey
+    /// </summary>
+    /// <param name="journeyId">The unique identifier for the desired recurrent journey</param>
+    /// <returns>A ready to use URI for deleting a specific recurrent journey</returns>
     public static Uri GetDeleteRecurrentJourneyUri(int journeyId)
     {
       return GetReadRecurrentJourneyUri(journeyId);
