7ccdc13d675d5eeae5697adfb99e64c9ae52b894
Andrea Panizza <panizza01@gmail.com>
2014-04-22 17:40:56 +0200

    added missing files

---
+++ Models/MobilityService/PublicTransport/LimitedTimeTable.cs
@@ -0,0 +1,55 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.MobilityService.PublicTransport
+{
+  public class LimitedTimeTable
+  {
+    [JsonProperty("delays")]
+    public Dictionary<string, Delay> Delays { get; set; }
+
+    [JsonProperty("name")]
+    public string RouteName { get; set; }
+
+    [JsonProperty("route")]
+    public string RouteShortName { get; set; }
+
+    [JsonProperty("times")]
+    public List<Time> Times { get; set; }
+
+    public override string ToString()
+    {
+      StringBuilder sb = new StringBuilder();
+      foreach (var proper in typeof(LimitedTimeTable).GetProperties())
+      {
+        sb.AppendFormat("{0}: {1}\n", proper.Name, proper.GetValue(this));
+      }
+      return sb.ToString();
+    }
+
+  }
+
+  public class Time
+  {
+    [JsonProperty("time")]
+    public long TimeInfo { get; set; }
+
+    [JsonProperty("trip")]
+    public Trip TripInfo { get; set; }
+
+    public override string ToString()
+    {
+      StringBuilder sb = new StringBuilder();
+      foreach (var proper in typeof(Time).GetProperties())
+      {
+        sb.AppendFormat("{0}: {1}\n", proper.Name, proper.GetValue(this));
+      }
+      return sb.ToString();
+    }
+
+  }
+}
---
+++ Models/MobilityService/PublicTransport/Trip.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.MobilityService.PublicTransport
+{
+  public class Trip
+  {
+    [JsonProperty("agency")]
+    [JsonConverter(typeof(StringEnumConverter))]
+    public AgencyType AgencyId { get; set; }
+
+    [JsonProperty("id")]
+    public string Id { get; set; }
+
+
+    public override string ToString()
+    {
+      StringBuilder sb = new StringBuilder();
+      foreach (var proper in typeof(Trip).GetProperties())
+      {
+        sb.AppendFormat("{0}: {1}\n", proper.Name, proper.GetValue(this));
+      }
+      return sb.ToString();
+    }
+  }
+}
