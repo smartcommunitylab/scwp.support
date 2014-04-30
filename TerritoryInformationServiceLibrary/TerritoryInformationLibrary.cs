1610d3877b2eec964cc3eb24240ae1145c39ecfd
TheFerrango <llorenzo@outlook.com>
2014-04-30 16:12:49 +0200

    added documentation
    *more documentation for the TerritoryInformation library

--- TerritoryInformationServiceLibrary/TerritoryInformationLibrary.cs
+++ TerritoryInformationServiceLibrary/TerritoryInformationLibrary.cs
@@ -9,19 +9,36 @@ using Newtonsoft.Json;
 
 namespace TerritoryInformationServiceLibrary
 {
-  public class TerritoryInformationLibrary
+  /// <summary>
+  /// Class that wraps the Territory Information API in an easy to use way
+  /// </summary>
+  public class TerritoryInformationLibrary 
   {
     HttpClient httpCli;
     string accessToken;
 
+    /// <summary>
+    /// Main constructor, to use always
+    /// </summary>
+    /// <param name="accessToken">The SmartCampus-issued access token</param>
     public TerritoryInformationLibrary(string accessToken)
     {
       this.accessToken = accessToken;
       httpCli = new HttpClient();
     }
 
+    /*
+     *  Series of functions that provide reading access to various territory related resources,
+     *  such as Events, Stories and Point of interes 
+     */
     #region Reading functions
 
+    
+    /// <summary>
+    ///  Asyncronous method that retrieves a list of all available events
+    /// </summary>
+    /// <param name="filterData">Instance of a FilterObject object that will be used as a search filter for the events</param>
+    /// <returns>A list containing the required events, up to a maximum of 100 results</returns>
     public async Task<List<EventObject>> ReadEvents(FilterObject filterData = null)
     {
       httpCli.DefaultRequestHeaders.Clear();
@@ -32,6 +49,11 @@ namespace TerritoryInformationServiceLibrary
       return JsonConvert.DeserializeObject<List<EventObject>>(JSONResult);
     }
 
+    /// <summary>
+    /// Asyncronous method that retrieves a single event
+    /// </summary>
+    /// <param name="eventId">The unique identifier for the required event</param>
+    /// <returns>An instance of an event containing the required event</returns>
     public async Task<EventObject> ReadSingleEvent(string eventId)
     {
       httpCli.DefaultRequestHeaders.Clear();
@@ -42,6 +64,11 @@ namespace TerritoryInformationServiceLibrary
       return JsonConvert.DeserializeObject<EventObject>(JSONResult);
     }
 
+    /// <summary>
+    ///  Asyncronous method that retrieves a list of all available POIs
+    /// </summary>
+    /// <param name="filterData">Instance of a FilterObject object that will be used as a search filter for the POIs</param>
+    /// <returns>A list containing the required POIs, up to a maximum of 100 results</returns>
     public async Task<List<POIObject>> ReadPlaces(FilterObject filterData = null)
     {
       httpCli.DefaultRequestHeaders.Clear();
@@ -52,6 +79,11 @@ namespace TerritoryInformationServiceLibrary
       return JsonConvert.DeserializeObject<List<POIObject>>(JSONResult);
     }
 
+    /// <summary>
+    /// Asyncronous method that retrieves a single POI
+    /// </summary>
+    /// <param name="placeId">The unique identifier for the required POI</param>
+    /// <returns>An instance of an event containing the required POI</returns>
     public async Task<POIObject> ReadSinglePlace(string placeId)
     {
       httpCli.DefaultRequestHeaders.Clear();
@@ -62,6 +94,11 @@ namespace TerritoryInformationServiceLibrary
       return JsonConvert.DeserializeObject<POIObject>(JSONResult);
     }
 
+    /// <summary>
+    ///  Asyncronous method that retrieves a list of all available stories
+    /// </summary>
+    /// <param name="filterData">Instance of a FilterObject object that will be used as a search filter for the stories</param>
+    /// <returns>A list containing the required stories, up to a maximum of 100 results</returns>
     public async Task<List<StoryObject>> ReadStories(FilterObject filterData = null)
     {
       httpCli.DefaultRequestHeaders.Clear();
@@ -72,6 +109,11 @@ namespace TerritoryInformationServiceLibrary
       return JsonConvert.DeserializeObject<List<StoryObject>>(JSONResult);
     }
 
+    /// <summary>
+    /// Asyncronous method that retrieves a single story
+    /// </summary>
+    /// <param name="storyId">The unique identifier for the required POI</param>
+    /// <returns>An instance of an event containing the required POI</returns>
     public async Task<StoryObject> ReadSingleStory(string storyId)
     {
       httpCli.DefaultRequestHeaders.Clear();
@@ -84,11 +126,20 @@ namespace TerritoryInformationServiceLibrary
 
     #endregion
 
+    /*
+     *  Series of functions that allow for the management of personalized (favourite) resources,
+     *  such as Events, Stories and Point of interes 
+     */
     #region Favourite object management
 
+    /// <summary>
+    /// Actually performs the network add operation and json deserialization
+    /// </summary>
+    /// <typeparam name="T">The object type to use</typeparam>
+    /// <param name="objectId">the unique identifier for the required resource</param>
+    /// <returns>an instance of the specified object type</returns>
     private async Task<T> AddToMyObjects<T>(string objectId)
     {
-
       httpCli.DefaultRequestHeaders.Clear();
       httpCli.DefaultRequestHeaders.Add("Accept", "application/json");
       httpCli.DefaultRequestHeaders.Add("Authorization", string.Format("Bearer {0}", accessToken));
@@ -98,16 +149,34 @@ namespace TerritoryInformationServiceLibrary
       return JsonConvert.DeserializeObject<T>(await JSONResult.Content.ReadAsStringAsync());
     }
 
+    /// <summary>
+    /// Asyncronous method that allows to add a specific story to the ones
+    /// one wants to add as favourite
+    /// </summary>
+    /// <param name="storyId">the unique identifier for the required story</param>
+    /// <returns>an instance of the requested story</returns>
     public async Task<StoryObject> AddToMyStories(string storyId)
     {
       return await AddToMyObjects<StoryObject>(storyId);
     }
 
+    /// <summary>
+    /// Asyncronous method that allows to add a specific event to the ones
+    /// one wants to add as favourite
+    /// </summary>
+    /// <param name="storyId">the unique identifier for the required event</param>
+    /// <returns>an instance of the requested event</returns>
     public async Task<EventObject> AddToMyEvents(string eventId)
     {
       return await AddToMyObjects<EventObject>(eventId);
     }
 
+    /// <summary>
+    /// Actually performs the network remove operation and json deserialization
+    /// </summary>
+    /// <typeparam name="T">The object type to use</typeparam>
+    /// <param name="objectId">the unique identifier for the required resource</param>
+    /// <returns>an instance of the removed object type</returns>
     private async Task<T> RemoveFromMyObjects<T>(string objectId)
     {
       httpCli.DefaultRequestHeaders.Clear();
@@ -119,11 +188,23 @@ namespace TerritoryInformationServiceLibrary
       return JsonConvert.DeserializeObject<T>(await JSONResult.Content.ReadAsStringAsync());
     }
 
+    /// <summary>
+    /// Asyncronous method that allows to remove a specific story to the ones
+    /// one has as favourites
+    /// </summary>
+    /// <param name="storyId">the unique identifier for the required story</param>
+    /// <returns>an instance of the removed story</returns>
     public async Task<StoryObject> RemoveFromMyStories(string storyId)
     {
       return await RemoveFromMyObjects<StoryObject>(storyId);
     }
 
+    /// <summary>
+    /// Asyncronous method that allows to remove a specific event to the ones
+    /// one has as favourites
+    /// </summary>
+    /// <param name="storyId">the unique identifier for the required event</param>
+    /// <returns>an instance of the removed event</returns>
     public async Task<EventObject> RemoveFromMyEvents(string eventId)
     {
       return await RemoveFromMyObjects<EventObject>(eventId);
@@ -131,8 +212,18 @@ namespace TerritoryInformationServiceLibrary
 
     #endregion
 
+    /*
+     *  Series of functions that provide a way to follow or unfollow specific resources,
+     *  such as Events, Stories and Point of interes, in order to recieve notifications 
+     */
     #region Follow/Unfollow object
 
+    /// <summary>
+    /// Actually performs the network following operation and json deserialization
+    /// </summary>
+    /// <typeparam name="T">The object type to use</typeparam>
+    /// <param name="objectId">the unique identifier for the required resource</param>
+    /// <returns>an instance of the specified object type</returns>
     private async Task<T> FollowObject<T>(string objectId)
     {
       httpCli.DefaultRequestHeaders.Clear();
@@ -144,21 +235,45 @@ namespace TerritoryInformationServiceLibrary
       return JsonConvert.DeserializeObject<T>(await JSONResult.Content.ReadAsStringAsync());
     }
 
+    /// <summary>
+    /// Asyncronous method that allows to start following a specific event, 
+    /// and thus enables notifications for said event
+    /// </summary>
+    /// <param name="storyId">the unique identifier for the required event</param>
+    /// <returns>an instance of the requested event</returns>
     public async Task<EventObject> FollowEvent(string eventId)
     {
       return await AddToMyObjects<EventObject>(eventId);
     }
 
+    /// <summary>
+    /// Asyncronous method that allows to start following a specific POI, 
+    /// and thus enables notifications for said POI
+    /// </summary>
+    /// <param name="storyId">the unique identifier for the required POI</param>
+    /// <returns>an instance of the requested POI</returns>
     public async Task<POIObject> FollowPlace(string placeId)
     {
       return await AddToMyObjects<POIObject>(placeId);
     }
 
+    /// <summary>
+    /// Asyncronous method that allows to start following a specific story, 
+    /// and thus enables notifications for said story
+    /// </summary>
+    /// <param name="storyId">the unique identifier for the required story</param>
+    /// <returns>an instance of the requested story</returns>
     public async Task<StoryObject> FollowStory(string storyId)
     {
       return await AddToMyObjects<StoryObject>(storyId);
     }
 
+    /// <summary>
+    /// Actually performs the network unfollow operation and json deserialization
+    /// </summary>
+    /// <typeparam name="T">The object type to use</typeparam>
+    /// <param name="objectId">the unique identifier for the required resource</param>
+    /// <returns>an instance of the specified object type</returns>
     private async Task<T> UnFollowObject<T>(string objectId)
     {
       httpCli.DefaultRequestHeaders.Clear();
@@ -170,16 +285,34 @@ namespace TerritoryInformationServiceLibrary
       return JsonConvert.DeserializeObject<T>(await JSONResult.Content.ReadAsStringAsync());
     }
 
+    /// <summary>
+    /// Asyncronous method that allows to stop following a specific event, 
+    /// and thus disables notifications for said event
+    /// </summary>
+    /// <param name="storyId">the unique identifier for the required event</param>
+    /// <returns>an instance of the requested event</returns>
     public async Task<EventObject> UnFollowEvent(string eventId)
     {
       return await UnFollowObject<EventObject>(eventId);
     }
 
+    /// <summary>
+    /// Asyncronous method that allows to stop following a specific POI, 
+    /// and thus disables notifications for said POI
+    /// </summary>
+    /// <param name="storyId">the unique identifier for the required POI</param>
+    /// <returns>an instance of the requested POI</returns>
     public async Task<POIObject> UnFollowPlace(string placeId)
     {
       return await UnFollowObject<POIObject>(placeId);
     }
 
+    /// <summary>
+    /// Asyncronous method that allows to stop following a specific story, 
+    /// and thus disables notifications for said story
+    /// </summary>
+    /// <param name="storyId">the unique identifier for the required story</param>
+    /// <returns>an instance of the requested story</returns>
     public async Task<StoryObject> UnFollowStory(string storyId)
     {
       return await UnFollowObject<StoryObject>(storyId);
@@ -187,10 +320,21 @@ namespace TerritoryInformationServiceLibrary
 
     #endregion
 
+    /*
+     *  Series of functions that provide a way to create, edit or delete custom resources,
+     *  such as Events, Stories and Point of interes, created by the current user
+     */
     #region User defined objects
 
     #region generic functions
 
+    /// <summary>
+    /// Actually performs the network creation operation and json deserialization 
+    /// </summary>
+    /// <typeparam name="GenObject">The object type to use</typeparam>
+    /// <param name="go">An instance of a GenObject object</param>
+    /// <param name="url">The URI at which the operation will be performed</param>
+    /// <returns>an instance of the created object</returns>
     private async Task<GenObject> CreateUserDefinedObject<GenObject>(GenObject go, Uri url)
     {
       StringContent sc = new StringContent(JsonConvert.SerializeObject(go));
@@ -203,6 +347,13 @@ namespace TerritoryInformationServiceLibrary
       return JsonConvert.DeserializeObject<GenObject>(await JSONResult.Content.ReadAsStringAsync());
     }
 
+    /// <summary>
+    /// Actually performs the network update operation and json deserialization 
+    /// </summary>
+    /// <typeparam name="GenObject">The object type to use</typeparam>
+    /// <param name="go">An instance of a GenObject object</param>
+    /// <param name="url">The URI at which the operation will be performed</param>
+    /// <returns>an instance of the updated object</returns>
     private async Task<GenObject> UpdateUserDefinedObject<GenObject>(GenObject go, Uri url)
     {
       StringContent sc = new StringContent(JsonConvert.SerializeObject(go));
@@ -215,6 +366,10 @@ namespace TerritoryInformationServiceLibrary
       return JsonConvert.DeserializeObject<GenObject>(await JSONResult.Content.ReadAsStringAsync());
     }
 
+    /// <summary>
+    /// Actually performs the network deletion operation. no return, no wait
+    /// </summary>
+    /// <param name="url">The URI at which the operation will be performed</param>
     private void DeleteUserDefinedObject(Uri url)
     {
       httpCli.DefaultRequestHeaders.Clear();
@@ -230,16 +385,30 @@ namespace TerritoryInformationServiceLibrary
 
     #region User defined event
 
+    /// <summary>
+    /// Asyncronous method that creates an user defined event
+    /// </summary>
+    /// <param name="eo">The instance of the event to create</param>
+    /// <returns>an instance of the created event</returns>
     public async Task<EventObject> CreateUserDefinedEvent(EventObject eo)
     {
       return await CreateUserDefinedObject<EventObject>(eo, TerritoryInformationUriHelper.GetCreateUserDefinedEventUri());
     }
 
+    /// <summary>
+    /// Asyncronous method that updates an user defined event
+    /// </summary>
+    /// <param name="eo">The instance of the event to update</param>
+    /// <returns>an instance of the updated event</returns>
     public async Task<EventObject> UpdateUserDefinedEvent(EventObject eo)
     {
       return await UpdateUserDefinedObject<EventObject>(eo, TerritoryInformationUriHelper.GetUpdateUserDefinedEventUri(eo.Id));
     }
 
+    /// <summary>
+    /// Asyncronous method that deletes an user defined event
+    /// </summary>
+    /// <param name="eventId">The unique identidier for the event to remove</param>
     public void DeleteUserDefinedEvent(string eventId)
     {
       DeleteUserDefinedObject(TerritoryInformationUriHelper.GetDeleteUserDefinedEventUri(eventId));
