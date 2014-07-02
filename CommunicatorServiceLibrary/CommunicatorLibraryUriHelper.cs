988aa98a6c2b9816a29b8639700d2255554c7283
TheFerrango <llorenzo@outlook.com>
2014-07-02 14:51:03 +0200

    much documentation

--- CommunicatorServiceLibrary/CommunicatorLibraryUriHelper.cs
+++ CommunicatorServiceLibrary/CommunicatorLibraryUriHelper.cs
@@ -13,11 +13,11 @@ using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Shapes;
 
-namespace TerritoryInformationServiceLibrary
+namespace CommunicatorServiceLibrary
 {
   /// <summary>
-  /// Helper class for the territory information library, contains static functions that 
-  /// generate already-formatted URIs for using within the territory information library
+  /// Helper class for the communicator service library, contains static functions that 
+  /// generate already-formatted URIs for using within the  communicator service library
   /// </summary>
   public static class CommunicatorLibraryUriHelper
   {
@@ -36,7 +36,21 @@ namespace TerritoryInformationServiceLibrary
     //static string GCM_SENDER_ID = "GCM_SENDER_ID";
     //static string GCM_REGISTRATION_ID_USER = "GCM_REGISTRATION_ID_USER";
 
+    /*
+     *  Methods that generate URIs to use for reading and managing notifications related 
+     *  to the application itself, that do not require an user identifier
+     */
+
     #region Actions by APP
+
+    /// <summary>
+    /// Creates a formatted URI to read all the application related notifications
+    /// </summary>
+    /// <param name="since">since date, in milliseconds (use 0 to get all notifications from begin of time)</param>
+    /// <param name="position">position in the result set</param>
+    /// <param name="count">number of desired results (use -1 to get all notifications)</param>
+    /// <param name="appId">the unique identifier for the application</param>
+    /// <returns>A ready to use URI to use in order to read all application related notifications on the server</returns>
     public static Uri GetNotificationsByApp(long since, int position, int count, string appId)
     {
       UriBuilder ub = new UriBuilder(string.Format("{0}/{1}/{2}/{3}", baseUrl, byApp, appId, notification));
@@ -50,32 +64,64 @@ namespace TerritoryInformationServiceLibrary
       return ub.Uri;
     }
 
+    /// <summary>
+    /// Creates a formatted URI to read a single application related notification
+    /// </summary>
+    /// <param name="id">the unique identifier for the notification</param>
+    /// <param name="appId">the unique identifier for the application</param>
+    /// <returns>A ready to use URI to use in order to read a specific application related notification</returns>
     public static Uri GetNotificationByAppUri(string id, string appId)
     {
       UriBuilder ub = new UriBuilder(string.Format("{0}/{1}/{2}/{3}", baseUrl, byApp, notification, id));
       return ub.Uri;
     }
 
-    public static Uri GetSyncNotificationsByAppUri(SyncData syncData, string appId)
+    /// <summary>
+    /// Creates a formatted URI to syncronize application related notifications 
+    /// </summary>
+    /// <param name="version">the version of the current SyncData object</param>
+    /// <param name="appId">the unique identifier for the application</param>
+    /// <returns>A ready to use URI for syncronizing the the application related notifications</returns>
+    public static Uri GetSyncNotificationsByAppUri(long version, string appId)
     {
-      UriBuilder ub = new UriBuilder(string.Format("{0}/{1}/{2}/{3}/{4}?since={5}", baseUrl, byApp, appId, notification, sync, syncData.Version));
+      UriBuilder ub = new UriBuilder(string.Format("{0}/{1}/{2}/{3}/{4}?since={5}", baseUrl, byApp, appId, notification, sync, version));
       return ub.Uri;
     }
 
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="id"></param>
+    /// <param name="appId">the unique identifier for the application</param>
+    /// <returns></returns>
     public static Uri GetUpdateNotificationByAppUri(string id, string appId)
     {
       UriBuilder ub = new UriBuilder(string.Format("{0}/{1}/{2}/{3}/{4}", baseUrl, byApp, appId, notification, id));
       return ub.Uri;
     }
 
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="id"></param>
+    /// <param name="appId">the unique identifier for the application</param>
+    /// <returns></returns>
     public static Uri GetDeleteNotificationByAppUri(string id, string appId)
     {
       UriBuilder ub = new UriBuilder(string.Format("{0}/{1}/{2}/{3}/{4}", baseUrl, byApp, appId, notification, id));
       return ub.Uri;
     }
+
     #endregion
 
+
+    /*
+     *  Methods that generate URIs to use for reading and managing notifications related 
+     *  to the user itself, that do require an user identifier (i.e. the token)
+     */
+
     #region Actions by USER
+
     public static Uri GetNotificationsByUserUri(long since, int position, int count)
     {
       UriBuilder ub = new UriBuilder(string.Format("{0}/{1}/{2}", baseUrl, byUser, notification));
@@ -94,10 +140,10 @@ namespace TerritoryInformationServiceLibrary
       UriBuilder ub = new UriBuilder(string.Format("{0}/{1}/{2}/{3}", baseUrl, byUser, notification, id));
       return ub.Uri;
     }
-    
-    public static Uri GetSyncNotificationsByUserUri(SyncData syncData)
+
+    public static Uri GetSyncNotificationsByUserUri(long version)
     {
-      UriBuilder ub = new UriBuilder(string.Format("{0}/{1}/{2}/{3}?since={4}", baseUrl, byUser, notification, sync, syncData.Version));
+      UriBuilder ub = new UriBuilder(string.Format("{0}/{1}/{2}/{3}?since={4}", baseUrl, byUser, notification, sync, version));
       return ub.Uri;
     }
 
@@ -107,6 +153,12 @@ namespace TerritoryInformationServiceLibrary
       return ub.Uri;
     }
 
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="id"></param>
+    /// <param name="appId">the unique identifier for the application</param>
+    /// <returns></returns>
     public static Uri GetDeleteNotificationByUserUri(string id, string appId)
     {
       UriBuilder ub = new UriBuilder(string.Format("{0}/{1}/{2}/{3}", baseUrl, byUser, notification, id));
@@ -114,7 +166,18 @@ namespace TerritoryInformationServiceLibrary
     }
     #endregion
 
+
+    /*
+     *  Other support functions 
+     */
+
     #region Other
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="appId">the unique identifier for the application</param>
+    /// <returns></returns>
     public static Uri GetRegisterAppUri(string appId)
     {
       UriBuilder ub = new UriBuilder(string.Format("{0}/{1}/{2}/{3}", baseUrl, register, byApp, appId));
